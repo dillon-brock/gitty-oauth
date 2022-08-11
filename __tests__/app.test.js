373438f0275893ed829719a6ee9521f44b0b3a08
Dillon Brock <mdillon1297@gmail.com>
2022-08-10 19:25:21 -0700

    added github login route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+jest.mock('../lib/services/github');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
